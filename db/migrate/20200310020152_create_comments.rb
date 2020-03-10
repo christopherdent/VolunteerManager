3bc26f73fb6f4d5e172daf63f5603745f766adfc
Christopher Dent <christopherdent01@gmail.com>
2020-03-09 22:02:13 -0400

    basic models revised

--- db/migrate/20200310020152_create_comments.rb
+++ db/migrate/20200310020152_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.text :content
-      t.resources :user
-      t.resources :volunteer
+      t.references :user, null: false, foreign_key: true
+      t.references :volunteer, null: false, foreign_key: true
 
       t.timestamps
     end
