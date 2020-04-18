0859e9ffd2763aa0e9c1631b0fc299355607acff
Christopher Dent <christopherdent01@gmail.com>
2020-04-18 16:08:10 -0400

    Updated Navbar and buttons CSS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,11 @@ class UsersController < ApplicationController
     @user = User.find_by_id(params[:id])
   end
 
-
+  def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect_to users_path
+    end
 
   private
 
