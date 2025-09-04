ae85ae4a1af590cc71eb40fbd0c9f91102a75563
= <christopher.dent@opscompass.com>
2025-09-04 12:00:33 -0400

    disable pidifle

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,8 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
+# Disable pidfile in container environments
+pidfile ENV.fetch("PIDFILE") { nil }
 
 environment ENV.fetch("RAILS_ENV") { "production" }
 
